9e9b64aeea9e1723f25228ad66c88ca3d5e86f4e
tesla <jachuwroblewski4e@gmail.com>
2025-01-31 08:04:40 +0100

    komprehencja vs normal + wykres, słowniki i tablice

--- main.py
+++ main.py
@@ -1,16 +1,71 @@
-# This is a sample Python script.
+import time
+import matplotlib.pyplot as plt
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def tablice():
+    number = [100, 1000, 10000, 100000, 1000000]
+    normal_times = []
+    comprehension_times = []
 
+    for i in number:
+        numbers = list(range(1, i))
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+        start = time.time()
+        tab = []
+        for num in numbers:
+            if num % 2 == 0:
+                tab.append(num ** 2)
+        end = time.time()
+        normal_times.append(end - start)
 
+        start1 = time.time()
+        tab1 = [num ** 2 for num in numbers if num % 2 == 0]
+        end1 = time.time()
+        comprehension_times.append(end1 - start1)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+        if tab == tab1:
+            print("Both are equal")
+        else:
+            print("Both are not equal")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    return number, normal_times, comprehension_times
+
+def slowniki():
+    number = [100, 1000, 10000, 100000, 1000000]
+    normal_times = []
+    comprehension_times = []
+
+    for i in number:
+        numbers = list(range(1, i))
+
+        start = time.time()
+        tab = {}
+        for num in numbers:
+            if num % 2 == 0:
+                tab[num] = num ** 2
+        end = time.time()
+        normal_times.append(end - start)
+
+        start1 = time.time()
+        tab1 = {num: num ** 2 for num in numbers if num % 2 == 0}
+        end1 = time.time()
+        comprehension_times.append(end1 - start1)
+
+        if tab == tab1:
+            print("Both are equal")
+        else:
+            print("Both are not equal")
+
+    return number, normal_times, comprehension_times
+
+number1, normal_times1, comprehension_times1 = tablice()
+number2, normal_times2, comprehension_times2 = slowniki()
+
+fig, ax = plt.subplots()
+ax.plot(number1, normal_times1, label="Tablice normal", color='orange')
+ax.plot(number1, comprehension_times1,'r--', label="Tablice comprehension", color='orange')
+ax.plot(number2, normal_times2,label="Slowniki normal", color='blue')
+ax.plot(number2, comprehension_times2,'r--', label="Slowniki comprehension", color='blue')
+ax.set_xlabel('Number of elements')
+ax.set_ylabel('Time (seconds)')
+ax.legend()
+plt.show()
